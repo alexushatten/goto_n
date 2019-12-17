efb78768399aaccba49113d7069de65375252a54
yerf123 <marfrey@student.ethz.ch>
2019-12-17 22:55:39 +0100

    Annotated goto_n_value_iteration

--- packages/goto_n/src/goto_n_value_iteration.py
+++ packages/goto_n/src/goto_n_value_iteration.py
@@ -1,6 +1,34 @@
 import numpy as np
 
+'''
+This file defines the functions that caluclate the cost matrix and the value iteration that we 
+use to determine the optimal path of the robot. 
+'''
+
 def cost_matrix(matrix_shape, all_movements_matrix, termination_point, all_bot_positions, current_bot_position):
+    """
+    This function defines a cost matrix for the movement of the duckiebot in each direction.
+    It caluclates the cost for each possible movement command. If one command is not possible in a 
+    given location, the cost is infinity. If the robot moves one node, the cost is 1. Reaching the 
+    termination node has a cost of zero (except if the termiantion state has already been taken
+    by another duckiebot).
+
+    Args:
+        matrix shape (:obj:'Tuple'): The shape of the node matrix
+        all_movement_matrix (:obj:'Matrix'): The matrix that defines all the movements that 
+            a duckiebot can undertake in specific node locations. 
+        termination_point (:obj:'Int32MultiArray'): The array that defines the termination node (in rows and columns)
+        all_bot_positions (:obj:'Int32MultiArray'): The array that defines the starting positions for all the localized duckiebots
+        current_bot_position (:obj:'Int32MultiArray'): The position of the bot specific bot for which the cost
+            matrix is to be calculated.
+
+    Returns:
+        cost_matrix (Matrix): The matrix defines the cost for the duckiebot to move in a given direction 
+    
+    Additional:
+        The multiplication by a factor of 2 represents the increase in granularity of the node_matrix
+    """
+    
     matrix_size = 2*matrix_shape[0]*2*matrix_shape[1]
     
     #Init the cost matrix
@@ -15,14 +43,18 @@ def cost_matrix(matrix_shape, all_movements_matrix, termination_point, all_bot_p
     termination_row = termination_point[0]
     termination_column = termination_point[1]
     termination_direction = termination_point[2]
+    
     #Add 0 cost to desired location
     cell = termination_row*2*matrix_shape[1] + termination_column
     cost_mat[cell,4] = 0
 
+    #extract current bot
     current_name = current_bot_position[0]
+    
+    #iterate through the remaining bots
     for other_bot_position in all_bot_positions:
-
         other_name = other_bot_position[0]
+        
         #Skip the bot you are currently calculating the map for
         if other_name == current_name:
             continue
@@ -36,18 +68,43 @@ def cost_matrix(matrix_shape, all_movements_matrix, termination_point, all_bot_p
     return cost_mat
 
 def value_iteration(matrix_shape, all_movements_matrix, cost_mat):
+    """
+    This function performs the value iteration for a given duckiebot. This is a Dynammic Programming
+    Algorithm that makes use of Markov Decision Process's in order to find the optimal sequences of 
+    steps that aims to minimize the defined cost. 
+
+    Args:
+        matrix shape (:obj:'Tuple'): The shape of the node matrix
+        all_movement_matrix (:obj:'Matrix'): The matrix that defines all the movements that 
+            a duckiebot can undertake in specific node locations. 
+        cost_mat: (:obj:'Matrix'): The matrix defines the cost that each duckiebot will accumulate
+            for movements in specific directions
+
+    Returns:
+        Optimal_movements (:obj:'Int32MultiArray'): These are the optimal movement commands for the
+            duckiebot that minimizes the cost to reach the termination state. 
+        Number_of_movements (:obj:'Int'): The total number of movements of the duckiebot to reach the termination
+    
+    Additional:
+        The multiplication by a factor of 2 represents the increase in granularity of the node_matrix
+    """
+
     matrix_size = 2*matrix_shape[0]*2*matrix_shape[1] #Size of all the number nodes
 
+    #initialize the necessary matricies
     number_of_movements = np.zeros((matrix_size,1)) 
     updated_number_of_moves = np.zeros((matrix_size,1))
     optimal_movements =np.zeros((matrix_size,1))
     movements_per_command=np.zeros((5,1))
     V_1=np.zeros((matrix_size,1))
+    
+    #define a position for infinity
     V_1[0]=float('inf')
    
     iterate = True
     iteration_value=0
 
+    #perform the value iteration
     while iterate == True:
         iteration_value+=1
         for i in range(0, matrix_size):
